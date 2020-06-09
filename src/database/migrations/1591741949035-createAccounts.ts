f174bba9c669d134e0dfdf89cfbb68cfee11803c
Marcelo Araujo <marcelo.edu2@gmail.com>
2020-06-09 20:22:12 -0300

    Configured Database with MySql + TypeORM + Docker

---
+++ src/database/migrations/1591741949035-createAccounts.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export default class createAccounts1591741949035 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'accounts',
+        columns: [
+          {
+            name: 'id',
+            type: 'varchar',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'date',
+            type: 'timestamp',
+            isNullable: false,
+          },
+        ],
+      }),
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('accounts')
+  }
+}
